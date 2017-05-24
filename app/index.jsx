4a5ea282772a723a8f58e012f0b884fd42fed62c
alirezab83 <apple@apples-MacBook-Pro.local>
2017-05-24 08:27:58 +0430

    upgrade to react-router-dom
    version 4 from react-router-dom compatible with webpack 2

--- app/index.jsx
+++ app/index.jsx
@@ -1,32 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-
-class Home extends React.Component {
-	render() {
-		return (
-			<div>ouuoo1</div>
-		);
-	}
-}
-
-
-class App extends React.Component {
-	render() {
-		const { children } = this.props;
-		return (
-			<div className='Content'>
-				<div>test</div>
-				{ children }
-			</div>
-    );
-  }
-}
+import App from './components/app.jsx';
 
 render(
-	<BrowserRouter>
-		<Route path = '/' component = { App } />
-	</BrowserRouter>,
+	<App />,
 	document.getElementById('root-id')
 );
